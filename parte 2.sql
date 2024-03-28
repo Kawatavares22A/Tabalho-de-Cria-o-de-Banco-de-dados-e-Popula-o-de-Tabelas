4d8c65d4212d116caa847ed73ec2a9eb6e61716b
Kawatavares22A <162051026+Kawatavares22A@users.noreply.github.com>
2024-03-28 14:05:11 -0300

    Atualizar o parte 2.sql

--- parte 2.sql
+++ parte 2.sql
@@ -1,10 +1,10 @@
 INSERT INTO cliente (nome, endereco, email, celular, data_nascimento )
-VALUES ('John Doe', '123 Main St', 'johndoe@example.com', '555-1234', '1980-01-01'),
-       ('Jane Smith', '456 Elm St', 'janesmith@example.com', '555-5678', '1990-02-02');
+VALUES ('Adalberto kiki', '199 Mn rua', 'luluoe@example.com', '559-7294', '2000-01-01'),
+       ('Janete', '48328 St', 'janeh@example.com', '555-528', '2000-02-02');
        
 INSERT INTO produtos (nome, preco, descricao, quantidade_em_estoque)
-VALUES ('Product 1', 10.99, 'This is product 1', 100),
-       ('Product 2', 15.99, 'This is product 2', 200);
+VALUES ('Produto 1', 10.99, 'isso é produto 1', 100),
+       ('Produto 2', 15.99, 'isso é produto 2', 200);
        
 INSERT INTO pedido (cliente_id, data_de_compra, valor_total, data_estimada_de_entrega)
 VALUES (1, '2023-03-27 10:00:00', 100.99, '2023-04-03'),
